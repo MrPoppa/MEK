ddb467276b581e36a65dcbf53541c1c82e58a085
Benjamin Bengtsson <benjaminbenjibengtsson@gmail.com>
2015-04-21 06:57:37 +0200

    Reverted back to earliest copy to undo BarcodeScan implementation

--- app/src/main/java/com/mr/poppa/mrelectrokeeper/MainActivity.java
+++ app/src/main/java/com/mr/poppa/mrelectrokeeper/MainActivity.java
@@ -1,17 +1,50 @@
 package com.mr.poppa.mrelectrokeeper;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button scanButton;
+    private TextView scanOutputWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scanOutputWindow = (TextView) findViewById(R.id.scannedTextView);
+
+        // Scanner
+        scanButton = (Button) findViewById(R.id.addDeviceButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String result = scanResult.getContents();
+            scanOutputWindow.setText(result);
+        }
+        // Added as test
+        // else continue with any other code you need in the method
+
     }
 
 
